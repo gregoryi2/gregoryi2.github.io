4854c0a865fb47a58aaf7b7b57ebe7e731edcd16
gregoryi2 <gregory.pereira2@Gmail.com>
2021-01-04 09:29:03 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-	if($(window).width() <=768) {
-	  	  $("#section4").removeClass("pl-5");
-	      $("#section4").addClass("pl-1");
-	      $("#section2").addClass("p-0");
-	  };
-});
+// $(document).ready(function() {
+// 	if($(window).width() <=768) {
+// 	  	  $("#section4").removeClass("pl-5");
+// 	      $("#section4").addClass("pl-1");
+// 	      $("#section2").addClass("p-0");
+// 	  };
+// });
